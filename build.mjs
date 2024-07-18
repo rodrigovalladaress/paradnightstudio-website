6c2e9eec5b34288800f8f396041aa572e95c61e8
rodrigovalladaress <dim3trodon@gmail.com>
2024-07-18 18:01:24 +0100

    Use local path to copy files

--- build.mjs
+++ build.mjs
@@ -1,8 +1,8 @@
 import fs from "fs/promises";
 
 async function build() {
-  await fs.copyFile("index.html", "dist/index.html");
-  await fs.copyFile("style.css", "dist/style.css");
+  await fs.copyFile("./index.html", "./dist/index.html");
+  await fs.copyFile("./style.css", "./dist/style.css");
 }
 
 build();
