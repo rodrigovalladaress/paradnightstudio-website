29b5614bff73354ffea88e6c45be2d2df59a76c7
rodrigovalladaress <dim3trodon@gmail.com>
2024-07-18 18:24:54 +0100

    Use process.cwd to get current directory

--- build.mjs
+++ build.mjs
@@ -1,8 +1,20 @@
 import fs from "fs/promises";
+import process from "process";
+import { join } from "path";
+
+function getPath(path) {
+  return join(process.cwd(), path);
+}
+
+function copy(from, to) {
+  console.log(from, "->", to);
+
+  return fs.copyFile(from, to);
+}
 
 async function build() {
-  await fs.copyFile("./index.html", "./dist/index.html");
-  await fs.copyFile("./style.css", "./dist/style.css");
+  await copy(getPath("./index.html"), getPath("./dist/index.html"));
+  await copy(getPath("./style.css"), getPath("./dist/style.css"));
 }
 
 build();
