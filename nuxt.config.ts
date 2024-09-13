3e05d96ab4ef68281db4af495cbb8b4316b35f97
rodrigovalladaress <dim3trodon@gmail.com>
2024-09-13 15:32:43 +0100

    Uninstall stylelint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/stylelint-module", "@nuxt/test-utils/module", "@nuxt/fonts"],
-  stylelint: {
-    config: {
-      extends: ["stylelint-config-standard-scss", "stylelint-config-standard-vue/scss"],
-      rules: {
-        "declaration-property-unit-allowed-list": {
-          "/^border/": ["px"],
-          "/^padding|^gap/": ["rem"],
-        },
-        "unit-allowed-list": ["%", "deg", "px", "rem", "ms", "fr", "vh", "vw"],
-        "no-empty-source": null,
-      },
-    },
-  },
+  modules: ["@nuxt/eslint", "@nuxt/test-utils/module", "@nuxt/fonts"],
 });
