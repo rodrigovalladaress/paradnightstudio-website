968a3aa5ecdd164796ff2904d50f365bcb95779c
rodrigovalladaress <dim3trodon@gmail.com>
2024-09-19 18:04:58 +0100

    Show mobile footer item

--- components/FooterItem.vue
+++ components/FooterItem.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>Copyright © {{ new Date().getFullYear() }} Paradnight Studio</div>
+  <div class="px-edge pt-[120px] pb-[10px] text-sm text-white bg-gradient">
+    <div>Copyright © {{ new Date().getFullYear() }} Paradnight Studio</div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "@/assets/styles/_variables.scss";
+
+.bg-gradient {
+  background: linear-gradient(
+    180deg,
+    #{mix($color-woodsmoke, transparent, 95%)} 0%,
+    #{mix($color-tuna, transparent, 95%)} 13%,
+    #{mix($color-purple, transparent, 95%)} 37%,
+    #{mix($color-barneypurple, transparent, 95%)} 63%,
+    #{mix($color-royalpurple, transparent, 95%)} 100%
+  );
+}
+</style>
