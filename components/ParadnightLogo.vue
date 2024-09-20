7101a862fa2f3e8008eaf1e61ab7c31d87e8bdc0
rodrigovalladaress <dim3trodon@gmail.com>
2024-09-20 09:56:46 +0100

    Show video on header logo

--- components/ParadnightLogo.vue
+++ components/ParadnightLogo.vue
@@ -3,13 +3,22 @@
    size of the scaled child -->
   <div class="w-fit mx-[-26.5px] my-[-32px] md:mx-[26.5px] md:my-[32px]">
     <div
-      class="relative border-solid border-[6px] border-black bg-white scale-[0.75] w-[212px] h-[256px] md:scale-[1.25]"
+      class="relative border-solid border-[6px] border-black bg-white scale-[0.75] w-[212px] h-[256px]
+        md:scale-[1.25]"
     >
-      <img
+      <video
         class="absolute top-[10px] left-[10px] size-[180px]"
-        src="https://paradnight-studio.sirv.com/paradnight-website/only_paradnight.png"
-        alt="Paradnight Studio Logo"
-      />
+        poster="https://paradnight-studio.sirv.com/paradnight-website/only_paradnight.png?w=320&h=320"
+        preload="metadata"
+        loop
+        autoplay
+        muted
+      >
+        <source
+          src="https://paradnight-studio.sirv.com/paradnight-website/paradnight_loop_320.mp4"
+          type="video/mp4"
+        />
+      </video>
 
       <div
         class="absolute bottom-[12px] w-full text-center font-heading"
