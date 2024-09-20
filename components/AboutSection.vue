99b5a1d87de3c7da699c5e1b88dd79b3b8f38755
rodrigovalladaress <dim3trodon@gmail.com>
2024-09-20 15:19:05 +0100

    Style about section for desktop

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,32 +1,45 @@
 <template>
-  <div class="pt-edge3x px-edge pb-[7.5rem] bg-gradient">
-    <h3 class="mb-4 text-h3 leading-none font-heading text-springsun">About</h3>
+  <div
+    class="flex justify-center pt-edge3x px-edge pb-[7.5rem] sm:py-[250px] bg-gradient"
+  >
+    <div class="w-full max-w-[37.5rem]">
+      <h3 class="mb-4 text-h3 leading-none font-heading text-springsun">About</h3>
 
-    <div class="mb-4 w-full h-[150px] overflow-clip sm:h-[150px] sm:w-[300px]">
-      <img
-        src="https://paradnight-studio.sirv.com/paradnight-website/paradnight_banner.png?canvas.height=300"
-        alt="Paradnights"
-        class="h-full w-auto max-w-max m-auto object-contain relative left-[50%] translate-x-[-50%]"
-      />
-    </div>
+      <div class="mb-4 w-full h-[150px] overflow-clip sm:h-[215px]">
+        <img
+          src="https://paradnight-studio.sirv.com/paradnight-website/paradnight_banner.png?canvas.height=300"
+          alt="Paradnights"
+          class="sm:hidden h-full w-auto max-w-max m-auto object-contain relative left-[50%]
+            translate-x-[-50%]"
+        />
 
-    <div class="text-xl space-y-[0.75rem] leading-[1.2] text-white">
-      <p>
-        Paradnight Studio is a small indie game studio. I (Rodrigo) do most of the design
-        and programming work, but I usually collaborate with
-        <a class="link" href="https://andreabaroni.com/" target="_blank">Andrea Baroni</a
-        >, composer and sound designer.
-      </p>
+        <img
+          src="https://paradnight-studio.sirv.com/paradnight-website/paradnight_banner.png?canvas.height=370"
+          alt="Paradnights"
+          class="hidden sm:block h-full w-auto max-w-max m-auto object-contain relative left-[50%]
+            translate-x-[-50%]"
+        />
+      </div>
 
-      <p>
-        The idea behind the studio is to create weird, horror and/or retro inspired games.
-        But not limited to that, as we don't know what ideas lurk in our brains.
-      </p>
+      <div class="text-xl space-y-[0.75rem] leading-[1.2] text-white">
+        <p>
+          Paradnight Studio is a small indie game studio. I (Rodrigo) do most of the
+          design and programming work, but I usually collaborate with
+          <a class="link" href="https://andreabaroni.com/" target="_blank"
+            >Andrea Baroni</a
+          >, composer and sound designer.
+        </p>
 
-      <p>
-        If you like our games, leaving a review on Steam or itch.io would make a huge
-        difference to this minuscule studio!
-      </p>
+        <p>
+          The idea behind the studio is to create weird, horror and/or retro inspired
+          games. But not limited to that, as we don't know what ideas lurk in our brains.
+        </p>
+
+        <p>
+          If you like our games, leaving a review on Steam or itch.io would make a huge
+          difference to this minuscule studio!
+        </p>
+      </div>
     </div>
   </div>
 </template>
