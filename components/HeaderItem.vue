e9ff58f6624a5cb754cd9c894715dea64e85a344
rodrigovalladaress <dim3trodon@gmail.com>
2024-09-20 15:04:23 +0100

    Show ellipse gradient after header on desktop

--- components/HeaderItem.vue
+++ components/HeaderItem.vue
@@ -5,11 +5,14 @@
   </div>
 
   <!-- Desktop header -->
-  <div
-    class="hidden lg:flex flex-col min-h-screen px-edge py-edge2x gap-[50px] justify-center
-      bg-gradient-desktop"
-  >
-    <HeaderItemDesktop />
+  <div class="hidden lg:block">
+    <div
+      class="flex flex-col min-h-screen px-edge py-edge2x gap-[50px] justify-center bg-gradient-desktop"
+    >
+      <HeaderItemDesktop />
+    </div>
+
+    <div class="h-[250px] mt-auto bg-gradient-desktop-below" />
   </div>
 </template>
 
@@ -26,6 +29,14 @@ import { HeaderItemMobile, HeaderItemDesktop } from "#components";
     #{mix($color-violetpink, transparent, 95%)} 0%,
     #{mix($color-royalpurple, transparent, 95%)} 30%,
     #{mix($color-woodsmoke, transparent, 95%)} 42%
+  );
+}
+
+.bg-gradient-desktop-below {
+  background: radial-gradient(
+    ellipse 150% 70% at top,
+    #{mix($color-woodsmoke, transparent, 95%)} 30%,
+    #{mix($color-tuna, transparent, 95%)} 90%
   );
 }
 </style>
